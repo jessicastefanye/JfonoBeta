6908de41df692aa7ef9976ddba42539a34b4e05f
Jessica <jessicastefanye123@gmail.com>
2025-05-27 08:27:44 -0300

    First commit

--- Paciente.java
+++ Paciente.java
@@ -1,5 +1,5 @@
 public class Paciente {
-    String nome;
-    
-    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
 }
---
+++ src/Paciente.java
@@ -0,0 +1,6 @@
+
+public class Paciente {
+    String nome;
+    
+    
+}
